63d908643bc33b57d72dcb4cf356bee2c2a9e78d
abshelkovnikov-oss <abshelkovnikov@gmail.com>
2026-08-12 20:56:33 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ import logging
 import psycopg2
 import calendar
 from datetime import datetime
-from telegram import Update
 from telegram import BotCommand 
 from telegram import InlineKeyboardButton
 from telegram import InlineKeyboardMarkup
